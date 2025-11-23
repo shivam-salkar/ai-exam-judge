57cf33b66486a3dcd5782360c6b43485c25132f0
Shivam Salkar <salkarshivam28@gmail.com>
2025-11-23 09:43:23 +0530

    Add UI elements for workspace

--- pages/Workspace.jsx
+++ pages/Workspace.jsx
@@ -6,15 +6,34 @@ function Workspace() {
 
   return (
     <div className="p-5 h-screen">
-      <div className="bg-[#1E1E1E] p-5 h-full rounded-xl overflow-hidden border border-gray-300 shadow">
-        <Editor
-          height="100%"
-          defaultLanguage="c"
-          theme="vs-dark"
-          defaultValue="//Write your code here."
-          value={value}
-          onchange={(value) => setValue(value)}
-        />
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center justify-between  bg-[#161a1f] p-2 rounded-t-2xl overflow-hidden border border-gray-300 shadow">
+            <div className="">
+              main.c
+            </div>
+
+            <button className="btn">Run</button>
+          </div>
+
+          <div className="bg-[#1E1E1E] p-5 h-[80vh] w-[50vw]  rounded-b-2xl overflow-hidden border border-gray-300 shadow">
+            <Editor
+              height="75vh"
+              defaultLanguage="c"
+              theme="vs-dark"
+              defaultValue="//Write your code here."
+              value={value}
+              onchange={(value) => setValue(value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <textarea
+            className="bg-black h-[80vh] ml-5 p-5 w-[50vw] rounded-2xl overflow-hidden border border-gray-300 shadow"
+            name=""
+            id=""></textarea>
+        </div>
       </div>
     </div>
   );
